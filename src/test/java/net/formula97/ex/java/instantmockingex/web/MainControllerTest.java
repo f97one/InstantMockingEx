e87cab7987508c77666b8a711956149107018fbb
HAJIME Fukuna <f97one@gmail.com>
2022-02-22 22:34:32 +0900

    mockito-inlineでInstant.now()をモッキングできるように修正。

--- src/test/java/net/formula97/ex/java/instantmockingex/web/MainControllerTest.java
+++ src/test/java/net/formula97/ex/java/instantmockingex/web/MainControllerTest.java
@@ -3,11 +3,17 @@ package net.formula97.ex.java.instantmockingex.web;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneId;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -34,9 +40,16 @@ class MainControllerTest {
 
     @Test
     void index() throws Exception {
-        mockMvc.perform(get("/"))
-                .andExpect(status().isOk())
-                .andExpect(content().json("{\"current_time\":\"2022-02-22 21:11:23\"}"))
-                .andReturn();
+        Clock clock = Clock.fixed(Instant.parse("2022-02-22T12:11:23Z"), ZoneId.of("Asia/Tokyo"));
+        Instant instant = Instant.now(clock);
+
+        try(MockedStatic<Instant> mockedInstant = Mockito.mockStatic(Instant.class)) {
+            mockedInstant.when(Instant::now).thenReturn(instant);
+
+            mockMvc.perform(get("/"))
+                    .andExpect(status().isOk())
+                    .andExpect(content().json("{\"current_time\":\"2022-02-22 21:11:23\"}"))
+                    .andReturn();
+        }
     }
 }
